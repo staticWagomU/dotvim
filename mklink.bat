e4178f5e439df95f89e6c98229cb99d7bd93cb3d
staticWagomU <to6wa17@gmail.com>
2024-01-16 15:43:18 +0900

    update: .git/はmklinkしない

--- mklink.bat
+++ mklink.bat
@@ -7,7 +7,9 @@ mklink /D %LOCALAPPDATA%\nvim %USERPROFILE%\dotvim\nvim
 REM vimのシンボリックリンクを作成
 REM dir /B /A:Dでディレクトリのシンボリックリンクを作成する
 for /f %%i in ('dir /B /A:D %USERPROFILE%\dotvim\vim') do (
-  mklink /D %USERPROFILE%\vimfiles\%%i %USERPROFILE%\dotvim\vim\%%i
+  if not %%i == .git (
+    mklink /D %USERPROFILE%\vimfiles\%%i %USERPROFILE%\dotvim\vim\%%i
+  )
 )
 
 REM dir /B /A-Dでファイルのシンボリックリンクを作成する

cbd3e8c5742698768cf9c407657d0acd300b8519
staticWagomU <to6wa17@gmail.com>
2024-01-16 09:19:05 +0900

    refactor: use vim.fs.normalize and vim.fs.joinpath

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,9 @@
 if require('utils').is_windows then
-	vim.g['denops#deno'] = os.getenv('USERPROFILE') .. '/.deno/bin/deno.exe'
+	local normalize = vim.fs.normalize
+	local joinpath = vim.fs.joinpath
+	local USERPROFILE = os.getenv('USERPROFILE')
+	local deno_path = normalize(joinpath(USERPROFILE, '.deno/bin/deno.exe'))
+	vim.g['denops#deno'] = deno_path
 end
 vim.loader.enable()
 require('setup')

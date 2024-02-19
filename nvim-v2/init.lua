36467ace7bad7b8adb9dd30976bc0b41a9edacbd
staticWagomU <to6wa17@gmail.com>
2024-02-20 07:53:29 +0900

    nvim-v2: lspまわりのマッピングを追加

--- nvim-v2/init.lua
+++ nvim-v2/init.lua
@@ -127,6 +127,24 @@ require('jetpack.packer').add {
           }
         end,
       }
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(_)
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+          vim.keymap.set('n', ';R', '<cmd>lua vim.lsp.buf.references()<CR>')
+          vim.keymap.set('n', ';d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+          vim.keymap.set('n', ';D', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+          vim.keymap.set('n', ';i', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+          vim.keymap.set('n', ';t', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+          vim.keymap.set('n', ';r', '<cmd>lua vim.lsp.buf.rename()<CR>')
+          vim.keymap.set('n', ';a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+          vim.keymap.set('n', ';e', '<cmd>lua vim.diagnostic.open_float()<CR>')
+          vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+          vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+        end,
+      })
+
+      vim.lsp.handlers['textDocument/publishDiagnostics'] =
+        vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
     end,
   },
   {

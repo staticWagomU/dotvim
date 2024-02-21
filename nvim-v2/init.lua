6b60da303c7b501ac7adf0ed055bbfec50c3de9f
staticWagomU <to6wa17@gmail.com>
2024-02-21 10:38:39 +0900

    nvim-v2: 漢は黙って1枚岩構成

--- nvim-v2/init.lua
+++ nvim-v2/init.lua
@@ -1,4 +1,59 @@
-require('options')
+-- {{{ options
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.mapleader = ' '
+
+local set = vim.opt
+set.backspace = { 'indent', 'eol', 'start' }
+set.clipboard = 'unnamedplus'
+set.expandtab = true
+set.fillchars = {
+  stl = '─',
+  stlnc = '─',
+  diff = '∙',
+  eob = ' ',
+  fold = '·',
+  horiz = '─',
+  horizup = '┴',
+  horizdown = '┬',
+  vert = '│',
+  vertleft = '┤',
+  vertright = '├',
+  verthoriz = '┼',
+}
+set.hidden = true
+set.ignorecase = true
+set.laststatus = 3
+set.list = true
+set.listchars = {
+  eol = '↴',
+  tab = '▷⋯',
+  trail = '»',
+  space = '⋅',
+  nbsp = '⦸',
+  extends = '»',
+  precedes = '«',
+}
+set.wrap = false
+set.shiftwidth = 2
+set.signcolumn = 'yes'
+set.softtabstop = 2
+set.tabstop = 2
+--- }}}
+
 
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }

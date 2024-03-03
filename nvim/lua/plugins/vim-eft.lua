a8576f6043e466419383876d975e8c3dbb4b916b
staticWagomU <to6wa17@gmail.com>
2024-03-03 20:36:44 +0900

    nvim: これもよかった

---
+++ nvim/lua/plugins/vim-eft.lua
@@ -0,0 +1,12 @@
+-- {{{ repo: 'https://github.com/hrsh7th/vim-eft' }}}
+-- lua_source {{{
+local maps = require('utils').maps
+
+maps({ 'n', 'x', 'o' }, {
+  { ';', '<Plug>(eft-repeat)', { noremap = false } },
+  { 'f', '<Plug>(eft-f)', { noremap = false } },
+  { 'F', '<Plug>(eft-F)', { noremap = false } },
+  { 't', '<Plug>(eft-t)', { noremap = false } },
+  { 'T', '<Plug>(eft-T)', { noremap = false } },
+})
+-- }}}

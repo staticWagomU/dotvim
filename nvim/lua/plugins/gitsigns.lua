762eab2be3290cf17ee2ae64ff1d5820e5def86b
staticWagomU <to6wa17@gmail.com>
2024-03-03 20:35:18 +0900

    nvim: gitsignsはいるよね

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,55 @@
+-- {{{ repo: 'https://github.com/lewis6991/gitsigns.nvim' }}}
+-- lua_source {{{
+local nmaps = require('utils').nmaps
+local map = require('utils').map
+local gitsigns = require('gitsigns')
+gitsigns.setup {
+  signcolumn = true,
+  numhl = true,
+  attach_to_untracked = true,
+}
+
+nmaps {
+  {
+    '[g',
+    function()
+      gitsigns.prev_hunk()
+    end,
+  },
+  {
+    ']g',
+    function()
+      gitsigns.next_hunk()
+    end,
+  },
+  {
+    '<C-g><C-p>',
+    function()
+      gitsigns.prev_hunk()
+    end,
+  },
+  {
+    '<C-g><C-r>',
+    function()
+      gitsigns.undo_stage_hunk()
+    end,
+  },
+  {
+    '<C-g><C-q>',
+    function()
+      gitsigns.prev_hunk()
+    end,
+  },
+  {
+    '<C-g><C-v>',
+    function()
+      gitsigns.blame_line()
+    end,
+  },
+}
+
+map({ 'n', 'x' }, '<C-g><C-a>', function()
+  gitsigns.stage_hunk()
+end)
+
+-- }}}

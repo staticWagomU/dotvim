d40cae33d48090a04885393fd1b27fc4f43c8213
staticWagomU <to6wa17@gmail.com>
2024-03-04 11:05:14 +0900

    nvim: denopathを設定

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
+if require('utils').is_windows then
+  local normalize = vim.fs.normalize
+  local joinpath = vim.fs.joinpath
+  local USERPROFILE = os.getenv('USERPROFILE')
+  local deno_path = normalize(joinpath(USERPROFILE, '.deno/bin/deno.exe'))
+  vim.g['denops#deno'] = deno_path
+end
 vim.loader.enable()
 require('setup_dpp')
 require('dpp_vim')

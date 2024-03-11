5bcd71da3048a7b688b5e28176480fd370995400
staticWagomU <to6wa17@gmail.com>
2024-03-12 08:59:02 +0900

    mklink: rename v2 to sub

--- mklink.sh
+++ mklink.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ln -s ~/dotvim/nvim ~/.config/nvim
-ln -s ~/dotvim/nvim-v2 ~/.config/nvim-v2
+ln -s ~/dotvim/nvim-sub ~/.config/nvim-sub
+ln -s ~/dotvim/nvim-mini ~/.config/nvim-mini
 ln -s ~/dotvim/vim/vimrc ~/.vim/vimrc

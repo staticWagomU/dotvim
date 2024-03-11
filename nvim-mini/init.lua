eaa13dee51eb408b7e2f75e7e72d4a688a474aae
staticWagomU <to6wa17@gmail.com>
2024-03-12 08:58:42 +0900

    nvim-mini: ittan

--- nvim-mini/init.lua
+++ nvim-mini/init.lua
@@ -9,6 +9,10 @@ if not vim.loop.fs_stat(mini_path) then
   vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
+vim.opt.runtimepath:append(vim.fs.normalize('~/dotvim/nvim-wagomu'))
+require('nvim-wagomu').setup()
+local opts = { noremap = true, silent = true }
+
 -- Set up 'mini.deps' (customize to your liking)
 require('mini.deps').setup({ path = { package = path_package } })
 
@@ -21,64 +25,10 @@ now(function()
   vim.o.termguicolors = true
   vim.cmd.colorscheme('habamax')
 end)
-now(function()
-  require('mini.notify').setup()
-  vim.notify = require('mini.notify').make_notify()
-end)
-now(function() require('mini.tabline').setup() end)
-now(function()
-  local statusline = require('mini.statusline')
-  --stylua: ignore
-  local active = function()
-    local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
-    local git           = statusline.section_git({ trunc_width = 75 })
-    -- Try out 'mini.diff'
-    local diff          = vim.b.minidiff_summary_string or ''
-    local diagnostics   = statusline.section_diagnostics({ trunc_width = 75 })
-    local filename      = statusline.section_filename({ trunc_width = 140 })
-    local fileinfo      = statusline.section_fileinfo({ trunc_width = 120 })
-    local location      = statusline.section_location({ trunc_width = 75 })
-    local search        = statusline.section_searchcount({ trunc_width = 75 })
 
-    return statusline.combine_groups({
-      { hl = mode_hl,                  strings = { mode } },
-      { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics } },
-      '%<', -- Mark general truncate point
-      { hl = 'MiniStatuslineFilename', strings = { filename } },
-      '%=', -- End left alignment
-      { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-      { hl = mode_hl,                  strings = { search, location } },
-    })
-  end
-  statusline.setup({ content = { active = active } })
-end)
-
--- Safely execute later
-later(function() require('mini.ai').setup() end)
-later(function() require('mini.comment').setup() end)
-later(function() require('mini.pick').setup() end)
-later(function() require('mini.surround').setup() end)
-later(function() require('mini.align').setup() end)
-later(function() require('mini.bracketed').setup() end)
-later(function()
-  require('mini.completion').setup({
-    lsp_completion = {
-      source_func = 'omnifunc',
-      auto_setup = false,
-      process_items = function(items, base)
-        -- Don't show 'Text' and 'Snippet' suggestions
-        items = vim.tbl_filter(function(x) return x.kind ~= 1 and x.kind ~= 15 end, items)
-        return MiniCompletion.default_process_items(items, base)
-      end,
-    },
-    window = {
-      info = { border = 'double' },
-      signature = { border = 'double' },
-    },
-  })
-end)
 later(function()
   require('mini.files').setup({ windows = { preview = true } })
+  vim.keymap.set('n', '<Leader>e', MiniFiles.open, opts)
   local minifiles_augroup = vim.api.nvim_create_augroup('ec-mini-files', {})
   vim.api.nvim_create_autocmd('User', {
     group = minifiles_augroup,
@@ -86,41 +36,107 @@ later(function()
     callback = function(args) vim.api.nvim_win_set_config(args.data.win_id, { border = 'double' }) end,
   })
 end)
-later(function() require('mini.indentscope').setup() end)
 
--- Use external plugins with `add()`
 now(function()
-  -- Add to current session (install if absent)
-  add('nvim-tree/nvim-web-devicons')
-  require('nvim-web-devicons').setup()
+  require('mini.notify').setup()
+  vim.notify = require('mini.notify').make_notify()
 end)
 
-later(function()
-  -- Supply dependencies near target plugin
-  add({ source = 'neovim/nvim-lspconfig', depends = { 'williamboman/mason.nvim' } })
-end)
+later(function() require('mini.comment').setup() end)
+
+
+-- now(function() require('mini.tabline').setup() end)
+-- now(function()
+--   local statusline = require('mini.statusline')
+--   --stylua: ignore
+--   local active = function()
+--     local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+--     local git           = statusline.section_git({ trunc_width = 75 })
+--     -- Try out 'mini.diff'
+--     local diff          = vim.b.minidiff_summary_string or ''
+--     local diagnostics   = statusline.section_diagnostics({ trunc_width = 75 })
+--     local filename      = statusline.section_filename({ trunc_width = 140 })
+--     local fileinfo      = statusline.section_fileinfo({ trunc_width = 120 })
+--     local location      = statusline.section_location({ trunc_width = 75 })
+--     local search        = statusline.section_searchcount({ trunc_width = 75 })
+--
+--     return statusline.combine_groups({
+--       { hl = mode_hl,                  strings = { mode } },
+--       { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics } },
+--       '%<', -- Mark general truncate point
+--       { hl = 'MiniStatuslineFilename', strings = { filename } },
+--       '%=', -- End left alignment
+--       { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+--       { hl = mode_hl,                  strings = { search, location } },
+--     })
+--   end
+--   statusline.setup({ content = { active = active } })
+-- end)
+--
+-- -- Safely execute later
+-- later(function() require('mini.ai').setup() end)
+-- later(function() require('mini.pick').setup() end)
+-- later(function() require('mini.surround').setup() end)
+-- later(function() require('mini.align').setup() end)
+-- later(function() require('mini.bracketed').setup() end)
+-- later(function()
+--   require('mini.completion').setup({
+--     lsp_completion = {
+--       source_func = 'omnifunc',
+--       auto_setup = false,
+--       process_items = function(items, base)
+--         -- Don't show 'Text' and 'Snippet' suggestions
+--         items = vim.tbl_filter(function(x) return x.kind ~= 1 and x.kind ~= 15 end, items)
+--         return MiniCompletion.default_process_items(items, base)
+--       end,
+--     },
+--     window = {
+--       info = { border = 'double' },
+--       signature = { border = 'double' },
+--     },
+--   })
+-- end)
+-- later(function() require('mini.indentscope').setup() end)
+--
+-- -- Use external plugins with `add()`
+-- now(function()
+--   -- Add to current session (install if absent)
+--   add('nvim-tree/nvim-web-devicons')
+--   require('nvim-web-devicons').setup()
+-- end)
+--
+-- later(function()
+--   -- Supply dependencies near target plugin
+--   add({ source = 'neovim/nvim-lspconfig', depends = { 'williamboman/mason.nvim' } })
+-- end)
+--
+-- later(function()
+--   add({
+--     source = 'nvim-treesitter/nvim-treesitter',
+--     -- Use 'master' while monitoring updates in 'main'
+--     checkout = 'master',
+--     monitor = 'main',
+--     -- Perform action after every checkout
+--     hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+--   })
+--   require('nvim-treesitter.configs').setup({
+--     ensure_installed = { 'lua', 'vimdoc' },
+--     highlight = { enable = true },
+--   })
+-- end)
+--
+-- later(function()
+--   add('lewis6991/gitsigns.nvim')
+--   require('gitsigns').setup()
+-- end)
+--
+-- later(function()
+--   add('williamboman/mason.nvim')
+--   require('mason').setup()
+-- end)
 
 later(function()
-  add({
-    source = 'nvim-treesitter/nvim-treesitter',
-    -- Use 'master' while monitoring updates in 'main'
-    checkout = 'master',
-    monitor = 'main',
-    -- Perform action after every checkout
-    hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
-  })
-  require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'lua', 'vimdoc' },
-    highlight = { enable = true },
-  })
-end)
-
-later(function()
-  add('lewis6991/gitsigns.nvim')
-  source('plugins/gitsigns.lua')
-end)
-
-later(function()
-  add('williamboman/mason.nvim')
-  require('mason').setup()
+  add('sainnhe/everforest')
+  vim.opt.bg = 'dark'
+  vim.cmd.colorscheme('everforest')
 end)

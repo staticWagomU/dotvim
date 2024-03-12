2bf3bc00db6a95edba0b1e8612f3d170a6684c2d
staticWagomU <to6wa17@gmail.com>
2024-03-12 20:23:08 +0900

    nvim-sub: nvim-wagomuを使うように修正

--- nvim-sub/init.lua
+++ nvim-sub/init.lua
@@ -7,62 +7,8 @@ if not vim.loop.fs_stat(jetpack_path) then
   vim.cmd('packadd vim-jetpack | helptags ALL')
   vim.cmd('echo "Installed `vim-jetpack`" | redraw')
 end
-
--- {{{ options
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwFileHandlers = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.mapleader = ' '
-
-local set = vim.opt
-set.backspace = { 'indent', 'eol', 'start' }
-set.clipboard = 'unnamedplus'
-set.expandtab = true
-set.fillchars = {
-  stl = '─',
-  stlnc = '─',
-  diff = '∙',
-  eob = ' ',
-  fold = '·',
-  horiz = '─',
-  horizup = '┴',
-  horizdown = '┬',
-  vert = '│',
-  vertleft = '┤',
-  vertright = '├',
-  verthoriz = '┼',
-}
-set.hidden = true
-set.ignorecase = true
-set.laststatus = 3
-set.list = true
-set.listchars = {
-  eol = '↴',
-  tab = '▷⋯',
-  trail = '»',
-  space = '⋅',
-  nbsp = '⦸',
-  extends = '»',
-  precedes = '«',
-}
-set.wrap = false
-set.shiftwidth = 2
-set.signcolumn = 'yes'
-set.softtabstop = 2
-set.tabstop = 2
---- }}}
+vim.opt.runtimepath:append(vim.fs.normalize('~/dotvim/nvim-wagomu'))
+require('nvim-wagomu').setup()
 
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
@@ -241,7 +187,7 @@ require('jetpack.packer').add {
   {
     'https://github.com/kawarimidoll/tuskk.vim',
     config = function()
-      local dict_path = vim.fs.normalize(plugins_path, '/skk-dev/dict')
+      local dict_path = vim.fs.normalize(plugins_path ..  '/dict')
       keymap({ 'i', 'c' }, '<C-j>', '<Cmd>call tuskk#toggle()<Cr>', opts)
       keymap('n', '<C-j>', 'a<Cmd>call tuskk#toggle()<Cr>', opts)
       vim.fn['tuskk#initialize'] {

e6d8a183de8b45f4cba12bd07c976e9a9e4bb55e
staticWagomU <to6wa17@gmail.com>
2024-03-14 12:02:20 +0900

    wagomu-box: フォーマットしたるわ！第２弾

--- wagomu-box/lua/wagomu-box/plugin-manager/mini.lua
+++ wagomu-box/lua/wagomu-box/plugin-manager/mini.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 function M.setup()
-  local path_package = vim.fn.stdpath('data') .. '/site/'
-  local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+  local path_package = vim.fn.stdpath("data") .. "/site/"
+  ocal mini_path = path_package .. "pack/deps/start/mini.nvim"
   if not vim.loop.fs_stat(mini_path) then
     vim.cmd('echo "Installing `mini.nvim`" | redraw')
-    local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path }
+    local clone_cmd = { "git", "clone", "--filter=blob:none", "https://github.com/echasnovski/mini.nvim", mini_path }
     vim.fn.system(clone_cmd)
-    vim.cmd('packadd mini.nvim | helptags ALL')
+    vim.cmd("packadd mini.nvim | helptags ALL")
     vim.cmd('echo "Installed `mini.nvim`" | redraw')
   end
 end

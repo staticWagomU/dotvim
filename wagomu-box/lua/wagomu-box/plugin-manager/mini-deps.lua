d183258d88c2383989fd6e72c132ce6f9858abec
staticWagomU <to6wa17@gmail.com>
2024-03-16 08:05:35 +0900

    wagomu-box: plugins_pathを追加

--- wagomu-box/lua/wagomu-box/plugin-manager/mini-deps.lua
+++ wagomu-box/lua/wagomu-box/plugin-manager/mini-deps.lua
@@ -1,7 +1,12 @@
 local M = {}
 
+if not _G.WagomuBox then
+  _G.WagomuBox = {}
+end
+
 function M.setup()
   local path_package = vim.fn.stdpath("data") .. "/site/"
+  WagomuBox.plugins_path = vim.fs.joinpath(path_package, "pack/deps/opt")
   local mini_path = path_package .. "pack/deps/start/mini.deps"
   if not vim.loop.fs_stat(mini_path) then
     vim.cmd('echo "Installing `mini.deps`" | redraw')

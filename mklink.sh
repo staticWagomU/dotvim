be96ef60d9d602518a7ea440c27aa9c208d1d150
staticWagomU <to6wa17@gmail.com>
2024-03-16 07:28:18 +0900

    mklink: add nvim-writing

--- mklink.sh
+++ mklink.sh
@@ -3,4 +3,5 @@
 ln -s ~/dotvim/nvim ~/.config/nvim
 ln -s ~/dotvim/nvim-sub ~/.config/nvim-sub
 ln -s ~/dotvim/nvim-mini ~/.config/nvim-mini
+ln -s ~/dotvim/nvim-writing ~/.config/nvim-writing
 ln -s ~/dotvim/vim/vimrc ~/.vim/vimrc

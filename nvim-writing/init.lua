2b1307939a4ba0862c15c3e737e1d28223603a8d
staticWagomU <to6wa17@gmail.com>
2024-03-16 07:53:39 +0900

    nvim-writing: update

--- nvim-writing/init.lua
+++ nvim-writing/init.lua
@@ -1,6 +1,5 @@
--- Set up 'mini.deps
 vim.opt.runtimepath:append(vim.fs.normalize('~/dotvim/wagomu-box'))
-require('wagomu-box.plugin-manager.mini').setup()
+require('wagomu-box.plugin-manager.mini-deps').setup()
 require('wagomu-box').setup()
 
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later

b3517db3dcdcf0b60c4a40cc0ab176e145a444ca
staticWagomU <to6wa17@gmail.com>
2024-03-18 11:07:10 +0900

    wagomu-box: blameをビジュアルモードでも叩けるようにした

--- wagomu-box/lua/wagomu-box/plugin-config/gitsigns.lua
+++ wagomu-box/lua/wagomu-box/plugin-config/gitsigns.lua
@@ -12,7 +12,7 @@ nmaps({
   { "<C-g><C-p>", gitsigns.preview_hunk },
   { "<C-g><C-r>", gitsigns.undo_stage_hunk },
   { "<C-g><C-q>", gitsigns.prev_hunk },
-  { "<C-g><C-v>", gitsigns.blame_line },
 })
 
 map({ "n", "x" }, "<C-g><C-a>", gitsigns.stage_hunk)
+map({ "n", "x" }, "<C-g><C-v>", gitsigns.blame_line)

cb46eea4631405eea518b964a1f8b56b666e2952
staticWagomU <to6wa17@gmail.com>
2024-03-20 21:29:55 +0900

    mklink: nvim-writingを追加

--- mklink.bat
+++ mklink.bat
@@ -10,6 +10,7 @@ mklink /D %LOCALAPPDATA%\nvim %USERPROFILE%\dotvim\nvim
 mklink /D %LOCALAPPDATA%\nvim-sub %USERPROFILE%\dotvim\nvim-sub
 mklink /D %LOCALAPPDATA%\nvim-mini %USERPROFILE%\dotvim\nvim-mini
 mklink /D %LOCALAPPDATA%\nvim-tmp %USERPROFILE%\dotvim\nvim-tmp
+mklink /D %LOCALAPPDATA%\nvim-writing %USERPROFILE%\dotvim\nvim-writing
 
 REM vimのシンボリックリンクを作成
 REM dir /B /A:Dでディレクトリのシンボリックリンクを作成する

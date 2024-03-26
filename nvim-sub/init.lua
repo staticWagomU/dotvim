bf7a044c3c1759dcd9d91cefe1400d6936905677
staticWagomU <to6wa17@gmail.com>
2024-03-26 22:15:57 +0900

    nvim-sub: gitsignsの設定追加

--- nvim-sub/init.lua
+++ nvim-sub/init.lua
@@ -5,6 +5,7 @@ require('options')
 require('keymaps')
 
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+local nmaps = require('wagomu-box.utils').nmaps
 
 now(function()
   add('https://github.com/vim-jp/vimdoc-ja')
@@ -12,6 +13,21 @@ end)
 
 later(function()
   add('https://github.com/lewis6991/gitsigns.nvim')
+  local gitsigns = require('gitsigns')
+  gitsigns.setup({
+    signcolumn = true,
+    numhl = true,
+    word_diff = true,
+  })
+  nmaps({
+    {'[g', gitsigns.next_hunk },
+    {']g', gitsigns.prev_hunk },
+    {'<C-g><C-a>', gitsigns.stage_hunk },
+    {'<C-g><C-r>', gitsigns.reset_hunk },
+    {'<C-g><C-p>', gitsigns.preview_hunk },
+    {'<C-g><C-v>', gitsigns.blame_line },
+    {'<C-g><C-q>', gitsigns.setqflist },
+  })
 end)
 
 later(function()

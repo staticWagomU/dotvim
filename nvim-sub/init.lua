7ce3b594df63b9a0cbc2ea57aa363c4c0f21c32b
staticWagomU <to6wa17@gmail.com>
2024-03-27 23:22:21 +0900

    nvim-sub: 🤐

--- nvim-sub/init.lua
+++ nvim-sub/init.lua
@@ -1,6 +1,14 @@
 vim.opt.runtimepath:append(vim.fs.normalize('~/dotvim/wagomu-box'))
 require('wagomu-box.plugin-manager.mini-deps').setup()
 
+-- ref: https://zenn.dev/kawarimidoll/articles/18ee967072def7
+vim.treesitter.start = (function(wrapped)
+  return function(bufnr, lang)
+    lang = lang or vim.fn.getbufvar(bufnr or '', '&filetype')
+    pcall(wrapped, bufnr, lang)
+  end
+end)(vim.treesitter.start)
+
 require('options')
 require('keymaps')
 

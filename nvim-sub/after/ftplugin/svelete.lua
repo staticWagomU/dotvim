eeae15f7bc174cdcb9b9ee0ffe0c6f073bdb13dd
staticWagomU <to6wa17@gmail.com>
2024-04-01 08:31:16 +0900

    nvim-sub: svelte使う予定はいまのところない

---
+++ nvim-sub/after/ftplugin/svelete.lua
@@ -0,0 +1,8 @@
+MiniDeps.now(function ()
+  MiniDeps.add('https://github.com/evanleck/vim-svelte')
+end)
+
+MiniDeps.now(function ()
+  MiniDeps.add('https://github.com/leafOfTree/vim-svelte-plugin')
+end)
+

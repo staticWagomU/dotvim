8e0a2cb84b4263d78cb663a783d76c182d6d9cac
staticWagomU <to6wa17@gmail.com>
2024-03-31 20:05:08 +0900

    nvim-sub: update

--- nvim-sub/after/ftplugin/astro.lua
+++ nvim-sub/after/ftplugin/astro.lua
@@ -1,7 +1,5 @@
-local add, now = MiniDeps.add, MiniDeps.now
-
-now(function ()
-  add('https://github.com/wuelnerdotexe/vim-astro')
+MiniDeps.now(function ()
+  MiniDeps.add('https://github.com/wuelnerdotexe/vim-astro')
   vim.g.astro_typescript = 'enable'
   vim.g.astro_stylus = 'enaable'
 end)

647399b0b228efd2f6fe1515f99d3473da1b6a5e
staticWagomU <to6wa17@gmail.com>
2024-04-01 18:06:10 +0900

    nvim-sub: lsp update

--- nvim-sub/init.lua
+++ nvim-sub/init.lua
@@ -585,11 +585,15 @@ later(function()
               pathStrict = true,
               path = { '?.lua', '?/init.lua' },
             },
+            completion = { callSnippet = 'Both' },
+            diagnostics = { globals = { 'vim' } },
+            telemetry = { enable = false },
             workspace = {
               library = vim.list_extend(vim.api.nvim_get_runtime_file('lua', true), {
                 '${3rd}/luv/library',
                 '${3rd}/busted/library',
                 '${3rd}/luassert/library',
+                vim.api.nvim_get_runtime_file('', true),
               }),
               checkThirdParty = 'Disable',
             },

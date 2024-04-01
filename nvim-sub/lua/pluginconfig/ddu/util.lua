c7fdef5b808db23d9221340eb185a60df2219b2d
staticWagomU <to6wa17@gmail.com>
2024-04-01 18:07:06 +0900

    nvim-sub: update

--- nvim-sub/lua/pluginconfig/ddu/util.lua
+++ nvim-sub/lua/pluginconfig/ddu/util.lua
@@ -1,14 +1,40 @@
 local M = {}
 
----@param settings table
-function M.patch_global(settings)
-  vim.fn["ddu#custom#patch_global"](settings)
+---@param action string
+---@param args table
+function M.do_action(action, args)
+  if args ~= nil then
+    vim.fn['ddu#ui#do_action'](action, args)
+  else
+    vim.fn['ddu#ui#do_action'](action)
+  end
 end
 
----@param name string
----@param settings table
-function M.patch_local(name, settings)
-  vim.fn["ddu#custom#patch_local"](name, settings)
+--- @param name string
+function M.start_local(name)
+  vim.fn['ddu#start'] {
+    name = name,
+  }
+end
+
+function M.start(...)
+  vim.fn['ddu#start'](...)
+end
+
+function M.load_config(...)
+  vim.fn['ddu#custom#load_config'](...)
+end
+
+function M.patch_global(...)
+  vim.fn['ddu#custom#patch_global'](...)
+end
+
+function M.patch_local(...)
+  vim.fn['ddu#custom#patch_local'](...)
+end
+
+function M.set_static_import_path()
+  vim.fn['ddu#set_static_import_path']()
 end
 
 return M

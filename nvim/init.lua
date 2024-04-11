c3a2c452063dca893084fa316980fde097eee91f
staticWagomU <to6wa17@gmail.com>
2024-04-11 22:48:49 +0900

    nvim: ぶんきできるよー

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,9 @@
 vim.loader.enable()
 vim.opt.runtimepath:append(vim.fs.normalize('~/dotvim/wagomu-box'))
--- vim.opt.runtimepath:append(vim.fs.normalize('~/dotvim/nvim/wagomu/denopstatusline'))
+local useDenopstatusline = false
+if useDenopstatusline then
+  vim.opt.runtimepath:append(vim.fs.normalize('~/dotvim/nvim/wagomu/denopstatusline'))
+end
 require('wagomu-box.plugin-manager.mini-deps').setup()
 local utils = require('wagomu-box.utils')
 
@@ -1139,58 +1142,59 @@ later(function()
   add('https://github.com/vim-jp/vimdoc-ja')
 end)
 
-later(function()
-  add('https://github.com/rebelot/heirline.nvim')
-  local comp = require('pluginconfig.heirline')
-  local utils = require('heirline.utils')
+if useDenopstatusline == false then
+  later(function()
+    add('https://github.com/rebelot/heirline.nvim')
+    local comp = require('pluginconfig.heirline')
+    local heirline_utils = require('heirline.utils')
 
-  require('heirline').load_colors(comp.setup_colors())
-  local aug = vim.api.nvim_create_augroup('Heirline', { clear = true })
-  vim.api.nvim_create_autocmd('ColorScheme', {
-    desc = 'Update Heirline colors',
-    group = aug,
-    callback = function()
-      local colors = comp.setup_colors()
-      utils.on_colorscheme(colors)
-    end,
-  })
-
-  require('heirline').setup {
-    statusline = utils.insert(
-      {
-        static = comp.stl_static,
-        hl = { bg = 'bg' },
-      },
-      comp.ViMode,
-      comp.lpad(comp.Git),
-      comp.lpad(comp.LSPActive),
-      comp.lpad(comp.Diagnostics),
-      { provider = '%=' },
-      comp.rpad(comp.FileType),
-      comp.Ruler
-    ),
-    winbar = {
-      comp.FullFileName,
-    },
-    opts = {
-      disable_winbar_cb = function(args)
-        local buf = args.buf
-        local ignore_buftype = vim.tbl_contains({ 'prompt', 'nofile', 'terminal', 'quickfix' }, vim.bo[buf].buftype)
-        local filetype = vim.bo[buf].filetype
-        local ignore_filetype = filetype == 'fugitive' or filetype == 'qf' or filetype:match('^git')
-        local is_float = vim.api.nvim_win_get_config(0).relative ~= ''
-        return ignore_buftype or ignore_filetype or is_float
+    require('heirline').load_colors(comp.setup_colors())
+    local aug = vim.api.nvim_create_augroup('Heirline', { clear = true })
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      desc = 'Update Heirline colors',
+      group = aug,
+      callback = function()
+        local colors = comp.setup_colors()
+        heirline_utils.on_colorscheme(colors)
       end,
-    },
-  }
+    })
 
-  vim.api.nvim_create_user_command('HeirlineResetStatusline', function()
-    vim.o.statusline = "%{%v:lua.require'heirline'.eval_statusline()%}"
-  end, {})
+    require('heirline').setup {
+      statusline = heirline_utils.insert(
+        {
+          static = comp.stl_static,
+          hl = { bg = 'bg' },
+        },
+        comp.ViMode,
+        comp.lpad(comp.Git),
+        comp.lpad(comp.LSPActive),
+        comp.lpad(comp.Diagnostics),
+        { provider = '%=' },
+        comp.rpad(comp.FileType),
+        comp.Ruler
+      ),
+      winbar = {
+        comp.FullFileName,
+      },
+      opts = {
+        disable_winbar_cb = function(args)
+          local buf = args.buf
+          local ignore_buftype = vim.tbl_contains({ 'prompt', 'nofile', 'terminal', 'quickfix' }, vim.bo[buf].buftype)
+          local filetype = vim.bo[buf].filetype
+          local ignore_filetype = filetype == 'fugitive' or filetype == 'qf' or filetype:match('^git')
+          local is_float = vim.api.nvim_win_get_config(0).relative ~= ''
+          return ignore_buftype or ignore_filetype or is_float
+        end,
+      },
+    }
 
-  -- Because heirline is lazy loaded, we need to manually set the winbar on startup
-  vim.opt_local.winbar = "%{%v:lua.require'heirline'.eval_winbar()%}"
-end)
+    vim.api.nvim_create_user_command('HeirlineResetStatusline', function()
+      vim.o.statusline = "%{%v:lua.require'heirline'.eval_statusline()%}"
+    end, {})
+
+    vim.opt_local.winbar = "%{%v:lua.require'heirline'.eval_winbar()%}"
+  end)
+end
 
 later(function()
   add('https://github.com/ptdewey/yankbank-nvim')

5ea346c48cc44536fd3007c9e63a90c6089175e4
staticWagomU <to6wa17@gmail.com>
2024-05-29 21:18:04 +0900

    nvim: dialを設定しなおしてる

--- nvim/init.lua
+++ nvim/init.lua
@@ -1013,33 +1013,51 @@ later(function()
   require('dial.config').augends:register_group {
     default = {
       augend.integer.alias.decimal,
-      augend.semver.alias.semver,
-      augend.date.alias['%Y/%m/%d'],
-      augend.date.alias['%Y-%m-%d'],
-      augend.date.alias['%m/%d'],
-      augend.date.alias['%-m/%-d'],
-      augend.date.alias['%H:%M:%S'],
-      augend.date.alias['%H:%M'],
-      augend.constant.alias.bool,
-      augend.constant.alias.ja_weekday,
-      augend.constant.alias.ja_weekday_full,
-      augend.constant.new { elements = { 'and', 'or' } },
-      augend.constant.new {
-        elements = { '&&', '||' },
-        word = false,
-      },
-      augend.constant.new { elements = { 'let', 'const' } },
-      augend.case.new {
-        types = {
-          'PascalCase',
-          'camelCase',
-          'kebab-case',
-          'snake_case',
-          'SCREAMING_SNAKE_CASE',
-        },
-      },
     },
   }
+
+
+  require("dial.config").augends:on_filetype {
+    typescript = {
+      augend.constant.new { elements = { "true", "false" } },
+      augend.constant.new { elements = { "let", "const" } },
+    },
+    lua = {
+      augend.constant.new { elements = { "true", "false" } },
+    },
+    markdown = {
+      augend.misc.alias.markdown_header,
+    }
+  }
+  --   default = {
+  --     augend.integer.alias.decimal,
+  --     augend.semver.alias.semver,
+  --     augend.date.alias['%Y/%m/%d'],
+  --     augend.date.alias['%Y-%m-%d'],
+  --     augend.date.alias['%m/%d'],
+  --     augend.date.alias['%-m/%-d'],
+  --     augend.date.alias['%H:%M:%S'],
+  --     augend.date.alias['%H:%M'],
+  --     augend.constant.alias.bool,
+  --     augend.constant.alias.ja_weekday,
+  --     augend.constant.alias.ja_weekday_full,
+  --     augend.constant.new { elements = { 'and', 'or' } },
+  --     augend.constant.new {
+  --       elements = { '&&', '||' },
+  --       word = false,
+  --     },
+  --     augend.constant.new { elements = { 'let', 'const' } },
+  --     augend.case.new {
+  --       types = {
+  --         'PascalCase',
+  --         'camelCase',
+  --         'kebab-case',
+  --         'snake_case',
+  --         'SCREAMING_SNAKE_CASE',
+  --       },
+  --     },
+  --   },
+  -- }
   nmaps {
     {
       '<C-a>',

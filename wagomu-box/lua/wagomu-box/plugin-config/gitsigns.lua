4bff02b2ca28f3a1cc8cd5e0785f5d0eb3e406e2
staticWagomU <to6wa17@gmail.com>
2024-06-03 13:10:27 +0900

    wagomu-box(gitsigns): mainにあわせた

--- wagomu-box/lua/wagomu-box/plugin-config/gitsigns.lua
+++ wagomu-box/lua/wagomu-box/plugin-config/gitsigns.lua
@@ -7,10 +7,12 @@ gitsigns.setup {
 }
 
 nmaps {
-  { '[g', gitsigns.prev_hunk },
-  { ']g', gitsigns.next_hunk },
-  { '<C-g><C-p>', gitsigns.preview_hunk },
+  { ']g',         gitsigns.next_hunk },
+  { '[g',         gitsigns.prev_hunk },
+  { '<C-g>a',     gitsigns.stage_buffer },
   { '<C-g><C-r>', gitsigns.undo_stage_hunk },
+  { '<C-g><C-d>', '<Cmd>Gitsigns diffthis ~<Cr>' },
+  { '<C-g><C-p>', gitsigns.preview_hunk },
   { '<C-g><C-q>', gitsigns.setqflist },
 }
 

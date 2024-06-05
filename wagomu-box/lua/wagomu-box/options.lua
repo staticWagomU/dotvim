71d30443fcc4e66d1826dd2235278f1a52a50cac
staticWagomU <to6wa17@gmail.com>
2024-06-05 09:43:12 +0900

    nvim: wagomu-boxといっしょ

--- wagomu-box/lua/wagomu-box/options.lua
+++ wagomu-box/lua/wagomu-box/options.lua
@@ -2,13 +2,18 @@ local M = {}
 
 function M.apply()
   vim.g.loaded_2html_plugin = 1
+  vim.g.loaded_fzf = 1
   vim.g.loaded_getscript = 1
   vim.g.loaded_getscriptPlugin = 1
   vim.g.loaded_gzip = 1
+  vim.g.loaded_logiPat = 1
+  vim.g.loaded_matchit = 1
+  vim.g.loaded_matchparen = 1
   vim.g.loaded_netrw = 1
   vim.g.loaded_netrwFileHandlers = 1
   vim.g.loaded_netrwPlugin = 1
   vim.g.loaded_netrwSettings = 1
+  vim.g.loaded_remote_plugins = 1
   vim.g.loaded_rrhelper = 1
   vim.g.loaded_tar = 1
   vim.g.loaded_tarPlugin = 1
@@ -19,11 +24,19 @@ function M.apply()
   vim.g.mapleader = ' '
 
   local set = vim.opt
-  set.backspace = { 'indent', 'eol', 'start' }
-  set.clipboard = 'unnamedplus'
+  set.backspace = {
+    'indent',
+    'eol',
+    'start'
+  }
+  set.clipboard = 'unnamedplus,unnamed'
+  set.encoding = 'utf-8'
   set.expandtab = true
+  set.expandtab = true
+  set.fileencoding = 'utf-8'
+  set.fileencodings = 'utf-8,euc-jp,cp932'
   set.fillchars = {
-    stl = '─',
+    stl = ' ',
     stlnc = '─',
     diff = '∙',
     eob = ' ',
@@ -36,8 +49,16 @@ function M.apply()
     vertright = '├',
     verthoriz = '┼',
   }
+  set.foldcolumn = '1'
+  set.foldenable = true
+  set.foldlevel = 99999
+  set.foldlevelstart = 99999
+  set.helplang = 'ja,en'
   set.hidden = true
+  set.hlsearch = true
   set.ignorecase = true
+  set.incsearch = true
+  set.laststatus = 3
   set.laststatus = 3
   set.list = true
   set.listchars = {
@@ -49,11 +70,12 @@ function M.apply()
     extends = '»',
     precedes = '«',
   }
-  set.wrap = false
   set.shiftwidth = 2
   set.signcolumn = 'yes'
+  set.smartcase = true
   set.softtabstop = 2
   set.tabstop = 2
+  set.wrap = false
 end
 
 return M

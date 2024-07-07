391a19b8fa3ae62c0739af4d935093596f1978ba
staticWagomU <to6wa17@gmail.com>
2024-07-07 21:14:55 +0900

    nvim: なんかかっこいいやつ

--- nvim/init.lua
+++ nvim/init.lua
@@ -1095,6 +1095,45 @@ later(function()
   end
 end)
 
+later(function()
+  add('https://github.com/rachartier/tiny-inline-diagnostic.nvim')
+  -- Default configuration
+  require('tiny-inline-diagnostic').setup({
+    signs = {
+      left = "",
+      right = "",
+      diag = "●",
+      arrow = "    ",
+      up_arrow = "    ",
+      vertical = " │",
+      vertical_end = " └"
+    },
+    hi = {
+      error = "DiagnosticError",
+      warn = "DiagnosticWarn",
+      info = "DiagnosticInfo",
+      hint = "DiagnosticHint",
+      arrow = "NonText",
+      background = "CursorLine",       -- Can be a highlight or a hexadecimal color (#RRGGBB)
+      mixing_color = "None",           -- Can be None or a hexadecimal color (#RRGGBB). Used to blend the background color with the diagnostic background color with another color.
+    },
+    blend = {
+      factor = 0.27,
+    },
+    options = {
+      -- The minimum length of the message, otherwise it will be on a new line.
+      softwrap = 15,
+      --- When overflow="wrap", when the message is too long, it is then displayed on multiple lines.
+      overflow = "wrap",
+      --- Enable it if you want to always have message with `after` characters length.
+      break_line = {
+        enabled = false,
+        after = 30,
+      }
+    }
+  })
+end)
+
 now(function()
   add('https://github.com/yuki-yano/fuzzy-motion.vim')
   nmap('<Leader><Leader>', '<Cmd>FuzzyMotion<CR>')

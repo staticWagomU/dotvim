62d71bca1518e84f9414f4b6b3d72eca6410a3b5
staticWagomU <to6wa17@gmail.com>
2024-09-19 13:34:35 +0900

    nvim(wtabbar): update

--- nvim/lua/wtabbar.lua
+++ nvim/lua/wtabbar.lua
@@ -3,13 +3,13 @@ vim.opt.tabline = ' '
 
 local lsp_names = function()
   local servers = vim.iter(vim.lsp.get_clients({ bufnr = 0 })):map(function(server) return server.name end):totable()
-  return table.concat(servers, ', ')
+  return table.concat(servers, ',')
 end
 
 local track_lsp = vim.schedule_wrap(function()
   local winid = vim.fn.bufwinid(0)
   if winid ~= -1 and vim.fn.line('$', winid) > 1 then
-    vim.fn.setwinvar(winid, '&tabline', '(' .. lsp_names() .. ')')
+    vim.fn.setwinvar(winid, '&tabline', 'LSP: ' .. lsp_names())
   end
 end)
 

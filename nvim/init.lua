e510ef782fbda999c1522491cae2ba3e22040af9
staticWagomU <to6wa17@gmail.com>
2024-09-27 17:52:25 +0900

    nvim: everforestからeverforest-nvimに移行

--- nvim/init.lua
+++ nvim/init.lua
@@ -1161,13 +1161,64 @@ end)
 
 
 now(function()
-  add('https://github.com/sainnhe/everforest')
+  -- add('https://github.com/sainnhe/everforest')
+  add('https://github.com/neanias/everforest-nvim')
   add('https://github.com/rebelot/kanagawa.nvim')
   add('https://github.com/sainnhe/edge')
   add('https://github.com/EdenEast/nightfox.nvim')
   add('https://github.com/ayu-theme/ayu-vim')
   vim.g.ayucolor = 'light'
   vim.opt.background = 'dark'
+---@diagnostic disable-next-line: missing-fields
+  require('everforest').setup {
+    italics = true,
+    disable_italic_comments = true,
+    ---By default, the colour of the sign column background is the same as the as normal text
+    ---background, but you can use a grey background by setting this to `'grey'`.
+    sign_column_background = 'none',
+    ---The contrast of line numbers, indent lines, etc. Options are `'high'` or
+    ---`'low'` (default).
+    ui_contrast = 'low',
+    ---Dim inactive windows. Only works in Neovim. Can look a bit weird with Telescope.
+    ---
+    ---When this option is used in conjunction with show_eob set to `false`, the
+    ---end of the buffer will only be hidden inside the active window. Inside
+    ---inactive windows, the end of buffer filler characters will be visible in
+    ---dimmed symbols. This is due to the way Vim and Neovim handle `EndOfBuffer`.
+    dim_inactive_windows = false,
+    ---Some plugins support highlighting error/warning/info/hint texts, by
+    ---default these texts are only underlined, but you can use this option to
+    ---also highlight the background of them.
+    diagnostic_text_highlight = true,
+    ---Which colour the diagnostic text should be. Options are `'grey'` or `'coloured'` (default)
+    diagnostic_virtual_text = 'coloured',
+    ---Some plugins support highlighting error/warning/info/hint lines, but this
+    ---feature is disabled by default in this colour scheme.
+    diagnostic_line_highlight = true,
+    ---By default, this color scheme won't colour the foreground of |spell|, instead
+    ---colored under curls will be used. If you also want to colour the foreground,
+    ---set this option to `true`.
+    spell_foreground = false,
+    ---Whether to show the EndOfBuffer highlight.
+    show_eob = true,
+    ---Style used to make floating windows stand out from other windows. `'bright'`
+    ---makes the background of these windows lighter than |hl-Normal|, whereas
+    ---`'dim'` makes it darker.
+    ---
+    ---Floating windows include for instance diagnostic pop-ups, scrollable
+    ---documentation windows from completion engines, overlay windows from
+    ---installers, etc.
+    ---
+    ---NB: This is only significant for dark backgrounds as the light palettes
+    ---have the same colour for both values in the switch.
+    float_style = 'bright',
+    ---Inlay hints are special markers that are displayed inline with the code to
+    ---provide you with additional information. You can use this option to customize
+    ---the background color of inlay hints.
+    ---
+    ---Options are `'none'` or `'dimmed'`.
+    inlay_hints_background = 'dimmed',
+  }
   require('kanagawa').setup {
     compile = true,
     transparent = true,

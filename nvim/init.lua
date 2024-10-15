70186439b8fb5ad19cc1298150bc754266840fdf
staticWagomU <to6wa17@gmail.com>
2024-10-15 09:35:30 +0900

    nvim: gitgraphをひょうじさせるやつ

--- nvim/init.lua
+++ nvim/init.lua
@@ -310,6 +310,33 @@ now(function()
   }
 end)
 
+later(function()
+  add('https://github.com/sindrets/diffview.nvim')
+end)
+
+later(function()
+  add{
+    source = 'https://github.com/isakbm/gitgraph.nvim',
+    depends = { 'https://github.com/sindrets/diffview.nvim' },
+  }
+
+  ---@type I.GGConfig
+  require('gitgraph').setup {
+    hooks = {
+      -- Check diff of a commit
+      on_select_commit = function(commit)
+        vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+        vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+      end,
+      -- Check diff from commit a -> commit b
+      -- on_select_range_commit = function(from, to)
+      --   vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+      --   vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+      -- end,
+    },
+  }
+end)
+
 -- =========================================
 -- | ファイラー
 -- =========================================

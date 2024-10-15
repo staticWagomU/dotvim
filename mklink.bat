ec0b7b6a7a828f316e908d3ef5e7c22a6d875ccf
staticWagomU <to6wa17@gmail.com>
2024-10-15 13:05:38 +0900

    mklink: nvim-wagomuを追加

--- mklink.bat
+++ mklink.bat
@@ -6,11 +6,14 @@ del %LOCALAPPDATA%\nvim
 del %LOCALAPPDATA%\nvim-sub
 del %LOCALAPPDATA%\nvim-mini
 del %LOCALAPPDATA%\nvim-tmp
+del %LOCALAPPDATA%\nvim-writing
+del %LOCALAPPDATA%\nvim-wagomu
 mklink /D %LOCALAPPDATA%\nvim %USERPROFILE%\dotvim\nvim
 mklink /D %LOCALAPPDATA%\nvim-sub %USERPROFILE%\dotvim\nvim-sub
 mklink /D %LOCALAPPDATA%\nvim-mini %USERPROFILE%\dotvim\nvim-mini
 mklink /D %LOCALAPPDATA%\nvim-tmp %USERPROFILE%\dotvim\nvim-tmp
 mklink /D %LOCALAPPDATA%\nvim-writing %USERPROFILE%\dotvim\nvim-writing
+mklink /D %LOCALAPPDATA%\nvim-wagomu %USERPROFILE%\dotvim\nvim-wagomu
 
 REM vimのシンボリックリンクを作成
 REM dir /B /A:Dでディレクトリのシンボリックリンクを作成する
--- mklink.sh
+++ mklink.sh
@@ -6,4 +6,5 @@ ln -s ~/dotvim/nvim-mini ~/.config/nvim-mini
 ln -s ~/dotvim/nvim-darkpowered ~/.config/nvim-darkpowered
 ln -s ~/dotvim/nvim-writing ~/.config/nvim-writing
 ln -s ~/dotvim/nvim-tmp ~/.config/nvim-tmp
+ln -s ~/dotvim/nvim-wagomu ~/.config/nvim-wagomu
 ln -s ~/dotvim/vim/vimrc ~/.vim/vimrc

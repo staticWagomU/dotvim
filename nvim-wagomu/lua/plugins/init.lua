fb286896cfd2fb97cf6ed808804bbf9288857c1b
staticWagomU <to6wa17@gmail.com>
2024-10-23 15:10:16 +0900

    nvim-wagomu: lazy.nvimでvim-fallを動かす

---
+++ nvim-wagomu/lua/plugins/init.lua
@@ -0,0 +1,9 @@
+return {
+	{ "https://github.com/folke/lazy.nvim" },
+	{
+		"https://github.com/lambdalisue/vim-fall",
+		dependencies = {
+			"https://github.com/vim-denops/denops.vim",
+		},
+	},
+}

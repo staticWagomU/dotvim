3ee42f69bdb8be475245cf20d88f14896b10278a
staticWagomU <to6wa17@gmail.com>
2025-08-17 07:38:26 +0900

    feat(nvim): プラグインマネージャー入れるところ分離しよう

---
+++ nvim/lua/wagomu/mini-deps.lua
@@ -0,0 +1,16 @@
+if not _G.WagomuBox then
+  _G.WagomuBox = {}
+end
+
+local path_package = vim.fn.stdpath('data') .. '/site/'
+WagomuBox.plugins_path = vim.fs.joinpath(path_package, 'pack/deps/opt')
+local mini_path = path_package .. 'pack/deps/start/mini.deps'
+if not vim.uv.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.deps`" | redraw')
+  local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.deps', mini_path }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.deps | helptags ALL')
+  vim.cmd('echo "Installed `mini.deps`" | redraw')
+end
+require('mini.deps').setup { path = { package = path_package } }
+

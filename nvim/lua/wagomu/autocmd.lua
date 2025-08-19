29c2c17d95406d0be208efc866675e1bc3c8c23a
staticWagomU <to6wa17@gmail.com>
2025-08-20 08:48:49 +0900

    feat(nvim): skkeletonで変換中にcursorlineを設定する
    kuuさんの設定を拝借させていただきます

--- nvim/lua/wagomu/autocmd.lua
+++ nvim/lua/wagomu/autocmd.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter', 'ColorScheme' }, {
-  group = MyAuGroup,
+  group = WagomuBox.MyAuGroup,
   pattern = '*',
   callback = function()
     local bg = vim.fn.synIDattr(vim.fn.hlID("Normal"), "bg#")
@@ -20,4 +20,34 @@ vim.api.nvim_create_autocmd({ 'WinEnter', 'BufEnter', 'ColorScheme' }, {
   end,
   once = true,
 })
+
+-- refs: https://github.com/kuuote/dotvim/blob/a522b18d59b05abee70cb97be22c6ca66c54a852/conf/plug/skkeleton.vim#L42-L58
+-- skkeleton用のcursorline制御
+local cursorline_phase = {
+	['input:okurinasi'] = true,
+	['input:okuriari'] = true,
+	['henkan'] = true,
+}
+
+vim.api.nvim_create_autocmd('User', {
+	group = WagomuBox.MyAuGroup,
+	pattern = 'skkeleton-handled',
+	callback = function()
+		local phase = vim.g['skkeleton#state'].phase
+		if cursorline_phase[phase] then
+			vim.opt_local.cursorline = true
+		else
+			vim.opt_local.cursorline = false
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+	group = WagomuBox.MyAuGroup,
+	pattern = 'skkeleton-disable-post',
+	callback = function()
+		vim.opt_local.cursorline = false
+	end,
+})
+
 

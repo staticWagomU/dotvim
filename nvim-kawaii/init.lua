7e14c1d0d26c7a121725e4cc54e33e06fadf4229
staticWagomU <to6wa17@gmail.com>
2026-01-02 06:20:42 +0900

    feat(nvim-kawaii) mini.indentscopeとindent-blanklineを導入
    indentは可愛いほうが良い

--- nvim-kawaii/init.lua
+++ nvim-kawaii/init.lua
@@ -139,6 +139,42 @@ later(function()
 end)
 
 later(function()
+	add('https://github.com/lukas-reineke/indent-blankline.nvim')
+	local highlight = {
+		"RainbowRed",
+		"RainbowYellow",
+		"RainbowBlue",
+		"RainbowOrange",
+		"RainbowGreen",
+		"RainbowViolet",
+		"RainbowCyan",
+	}
+
+	local hooks = require "ibl.hooks"
+	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+	end)
+
+	require("ibl").setup {
+		indent = {
+			highlight = highlight,
+		},
+		scope = {
+			enabled = false,
+		}
+	}
+end)
+later(function()
+	require('mini.indentscope').setup()
+end)
+
+later(function()
 	add('https://github.com/stevearc/oil.nvim')
 	require('plugins.oil')
 end)

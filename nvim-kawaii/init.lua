95ef746948b2e9a687baf4a21e5d481db22e0aae
staticWagomU <to6wa17@gmail.com>
2026-01-02 07:00:10 +0900

    feat: 色をみなおし

--- nvim-kawaii/init.lua
+++ nvim-kawaii/init.lua
@@ -147,13 +147,13 @@ later(function()
 
 	local hooks = require "ibl.hooks"
 	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#BF0021" })
 		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#465AA4" })
+		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#CCA478" })
+		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#3A684A" })
+		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#BE79BB" })
+		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#739797" })
 	end)
 
 	require("ibl").setup {
